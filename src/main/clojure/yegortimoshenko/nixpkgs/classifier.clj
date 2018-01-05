a6522a07fdef905a18615c70f97c25eafef4935b
Yegor Timoshenko <yegortimoshenko@gmail.com>
2018-01-05 03:18:06 +0000

    Initialize

---
+++ src/main/clojure/yegortimoshenko/nixpkgs/classifier.clj
@@ -0,0 +1,44 @@
+(ns yegortimoshenko.nixpkgs.classifier
+  (:gen-class :implements [com.amazonaws.services.lambda.runtime.RequestHandler])
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [yegortimoshenko.io.http :as http]))
+
+(defn simplify-rule [rule]
+  (if (string? rule)
+    (re-pattern (str "(?i)(\\s|^)" (str/lower-case rule) "(\\s|$)"))
+    (identity rule)))
+
+(defn match-rule [title rule]
+  (re-find (simplify-rule rule) title))
+
+(defn read-ruleset []
+  (read-string (slurp (io/resource "ruleset.edn"))))
+
+(defn filter-by-val [pred m]
+  (filter (fn [[k v]] (pred v)) m))
+
+(defn classify [title]
+  (map first (filter-by-val (partial some (partial match-rule title)) (read-ruleset))))
+
+(defn github-classify [token {:strs [issue pull_request]}]
+  (let [{:strs [title url]} (or issue pull_request)]
+    (http/request {:body (json/write-str (classify title))
+                   :headers {:authorization [(str "token " token)]}}
+                  (str url "/labels"))))
+
+(defn new-ticket? [{:strs [action]}]
+  (= action "opened"))
+
+(defn valid-signature? [body secret signature] true)
+
+(defn handle [{:strs [body headers]}]
+  (let [{:strs [X-Hub-Signature]} headers
+        {:strs [GITHUB_SECRET GITHUB_TOKEN]} (System/getenv)
+        payload (json/read-str body)]
+    (when (and (valid-signature? body GITHUB_SECRET X-Hub-Signature)
+               (new-ticket? payload))
+      (github-classify GITHUB_TOKEN payload))))
+
+(defn -handleRequest [_ event ctx] (handle event))
